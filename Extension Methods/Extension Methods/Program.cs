f9723acfa15bc976140bbcf3d0003b23c20be879
Yogesh Yadav <108536566+26081yogesh@users.noreply.github.com>
2023-11-22 12:24:06 +0530

    Add files via upload
    Second Commit

---
+++ Extension Methods/Extension Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class ExtensionMethods
+{
+    public void M1()
+    {
+        Console.WriteLine("M1");
+    }
+    public void M2()
+    {
+        Console.WriteLine("M2");
+    }
+    public void M3()
+    {
+        Console.WriteLine("M3");
+    }
+}
+
+static class NewMethodClass
+{
+    public static void M4(this ExtensionMethods obj)
+    {
+        Console.WriteLine("M4");
+    }
+    public static void M5(this ExtensionMethods obj)
+    {
+        Console.WriteLine("M5");
+    }
+}
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Main Method");
+        ExtensionMethods obj = new ExtensionMethods();
+        obj.M1();
+        obj.M2();
+        obj.M3();
+        obj.M4();
+        obj.M5();
+    }
+}
